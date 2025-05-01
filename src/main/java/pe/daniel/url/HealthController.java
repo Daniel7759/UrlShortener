f5d7f1ad0bd3f3d27e20dc36fbac37dd2ca65fca
Daniel <danieljh7759@gmail.com>
2025-05-01 15:20:53 -0500

    se agrego endpoint para redireccionar solo con la url base y el shortCode

--- src/main/java/pe/daniel/url/HealthController.java
+++ src/main/java/pe/daniel/url/HealthController.java
@@ -3,13 +3,29 @@ package pe.daniel.url;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 public class HealthController {
 
+    private final UrlService urlService;
+
+    public HealthController(UrlService urlService) {
+        this.urlService = urlService;
+    }
+
     @GetMapping("/health")
     public ResponseEntity<?> health() {
         return new ResponseEntity<>("Servicio en linea", HttpStatus.OK);
+    }
+
+    @GetMapping("/{shortCode}")
+    public RedirectView redirectToOriginalUrl(@PathVariable(value = "shortCode") String shortCode){
+        Url urlDB = urlService.getShortUrl(shortCode);
+        RedirectView redirectView = new RedirectView();
+        redirectView.setUrl(urlDB.getUrl());
+        return redirectView;
     }
 }
